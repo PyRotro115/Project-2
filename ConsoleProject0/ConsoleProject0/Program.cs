3dd7b80a0d1f5b54e42c43a1a146d584fc5f55b4
Irvin Contreras <contrerasasirvin.05@hotmail.com>
2018-04-20 13:24:08 -0600

    Create a new project

---
+++ ConsoleProject0/ConsoleProject0/Program.cs
@@ -0,0 +1,29 @@
+﻿// File/Project Prolog
+// Name:     <PUT YOUR NAME HERE>
+// Period:   <PUT YOUR PERIOD HERE>
+// Username: contrirv000
+// Project:  ConsoleProject0
+// Date:     4/20/2018 1:21:37 PM
+//
+// I declare that the following code was written by me or provided 
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+
+using System;
+
+namespace ConsoleProject0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your code goes below here
+
+            // Your code goes above here
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
