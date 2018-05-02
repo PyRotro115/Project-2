c06eec951ef84ac5c039152c525bdc7253a3264f
AndyNyei-Legend <482876@gmail.com>
2018-05-02 13:29:46 -0600

    I changed some things

---
+++ ConsoleProject0/ConsoleProject0/Michael.cs
@@ -0,0 +1,15 @@
+﻿class Michael
+{
+    public int Strength;
+    public int Intelligence;
+    public int socialWellBeing;
+
+    public Michael(int S, int I, int sWB)
+    {
+        S = Strength;
+        I = Intelligence;
+        sWB = socialWellBeing;
+    }
+
+
+}
