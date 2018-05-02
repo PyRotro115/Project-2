f4b7a8e1c7dc7e014dc617626beaeb5c9fb9632b
Irvin Contreras <contrerasasirvin.05@hotmail.com>
2018-05-02 14:05:50 -0600

    Irvin's changes

---
+++ ConsoleProject0/ConsoleProject0/Michel.cs
@@ -0,0 +1,14 @@
+﻿class Michael
+{
+    public int Strength;
+    public int Intelligence;
+    public int socialWellBeing;
+
+    public Michael(int Strength, int Intelligence, int socialWellBeing)
+    {
+        this.Strength = Strength;
+        this.Intelligence = Intelligence;
+        this.socialWellBeing = socialWellBeing;
+    }
+
+}
